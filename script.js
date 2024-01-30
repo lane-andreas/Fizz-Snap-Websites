4e59c965e3fff75b0f15372d67fa9453cd8b980b
lane-andreas <laaaandreas@gmail.com>
2024-01-30 05:38:39 -0600

    l

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+let typeSplit = new SplitType(".fade", {
+  types: "lines, words, chars",
+  tagName: "span",
+});
+
+gsap.from(".fade .char", {
+  scale: ".8",
+  y: "50%",
+  opacity: 0,
+  ease: "power1.out",
+  stagger: 0.05,
+  delay: 0.5,
+  scrollTrigger: {
+    trigger: ".scroll",
+    start: "top center",
+  },
+});
+
 function isScrollDown() {
   return window.scrollY > window.innerHeight / 2;
 }
@@ -19,7 +37,7 @@ document.querySelectorAll(".menu").forEach((button) => {
       .trim();
 
     if (valueRight === "-100%") {
-      document.documentElement.style.setProperty("--slide-nav", "7vw");
+      document.documentElement.style.setProperty("--slide-nav", "1em");
     } else {
       document.documentElement.style.setProperty("--slide-nav", "-100%");
     }
